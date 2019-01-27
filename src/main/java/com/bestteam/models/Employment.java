5908d8c6a53547f80e1cfc2e2bcb82e0438dd951
Kimi450 <karan_samani@yahoo.co.in>
2019-01-27 17:58:47 +0000

    Added constructors, setters and getters

--- src/main/java/com/bestteam/models/Employment.java
+++ src/main/java/com/bestteam/models/Employment.java
@@ -6,6 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import javax.persistence.GenerationType;
 
 @Entity
 @Table(name="Employment")
@@ -23,5 +24,47 @@ public class Employment {
     @NotNull
     private int years;
 
-    
+    public Employment() {
+        super();
+    }
+
+    public Employment(Long id, String institution, String location, int years) {
+        super();
+        this.id = id;
+        this.institution = institution;
+        this.location = location;
+        this.years = years;
+    }
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getInstitution() {
+		return institution;
+	}
+
+	public void setInstitution(String institution) {
+		this.institution = institution;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public int getYears() {
+		return years;
+	}
+
+	public void setYears(int years) {
+		this.years = years;
+	}
+
 }
