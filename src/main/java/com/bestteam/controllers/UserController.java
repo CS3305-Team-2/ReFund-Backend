e66729b0b60b28eabd8a22c362c639dc6515a3b8
Kimi450 <karan_samani@yahoo.co.in>
2019-01-31 18:17:13 +0000

    Added endpoint for /api/users/userId/educations which is getting all educations for a user id

--- src/main/java/com/bestteam/controllers/UserController.java
+++ src/main/java/com/bestteam/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Optional;
 
 import javax.validation.Valid;
@@ -16,7 +17,9 @@ import javax.validation.Valid;
 import com.bestteam.exceptions.UserNotFoundException;
 import com.bestteam.helpers.Response;
 import com.bestteam.models.User;
+import com.bestteam.models.Education;
 import com.bestteam.repository.UserRepository;
+import com.bestteam.repository.EducationRepository;
 
 @RestController
 @RequestMapping("/api/users")
@@ -24,12 +27,15 @@ public class UserController {
 
     @Autowired
     private UserRepository repository;
-    
+
+    @Autowired
+    private EducationRepository educationRepository;
+
     @GetMapping
     public List<User> getUserCollection() {
         return (List<User>)repository.findAll();
     }
-    
+
     // TODO more than this lmao
     @PostMapping
     public void createUser(@Valid @RequestBody User user) {
@@ -43,5 +49,12 @@ public class UserController {
             throw new UserNotFoundException(userId.toString());
         }
         return new Response<>(user.get());
+    }
+
+    @GetMapping("/{userId}/educations")
+    public Response<List<Education>> getUserEducations(@PathVariable("userId") Long userId) {
+        List<Education> list = new ArrayList<>();
+        educationRepository.findByEducationIdentityUserId(userId).forEach(list::add);
+        return new Response<>(list);
     }
 }
