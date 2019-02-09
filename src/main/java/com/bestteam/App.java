d25c7bbdb590f21e7e2b472c1580be12a186a03e
Kimi450 <karan_samani@yahoo.co.in>
2019-02-09 12:20:44 +0000

    Work on the backend for uploading files should be done.

--- src/main/java/com/bestteam/App.java
+++ src/main/java/com/bestteam/App.java
@@ -1,9 +1,15 @@
 package com.bestteam;
 
+import com.bestteam.helpers.FileStorageProperties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
