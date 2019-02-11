24e15678dd73ee4d8850bf901d4a52fe56a33e3f
Kimi450 <karan_samani@yahoo.co.in>
2019-02-11 15:13:56 +0000

    Added tables from other branches

---
+++ src/main/resources/db/migration/V4__schema.sql
@@ -0,0 +1,53 @@
+ALTER TABLE `Funding` RENAME TO `Grant`;
+
+ALTER TABLE `TeamMembers` drop CONSTRAINT team_member_to_grant;
+ALTER TABLE `TeamMembers` add CONSTRAINT team_member_to_grant
+                        FOREIGN KEY `primary_attribution`
+                        REFERENCES `Grant`(`id`);
+
+
+CREATE TABLE `Impact`
+(
+    `id` INT AUTO_INCREMENT NOT NULL,
+    `title` VARCHAR(50) NOT NULL,
+    `category` VARCHAR(60) NOT NULL,
+    `primary_beneficiary` INT NOT NULL,
+    `primary_attribution` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY impacts_to_user(`primary_beneficiary`)
+    REFERENCES `User`(`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+    FOREIGN KEY impacts_to_grants(`primary_attribution`)
+    REFERENCES `Grant`(`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+
+CREATE TABLE `Presentations`
+(
+    `id` INT AUTO_INCREMENT,
+    `year` INT NOT NULL,
+    `title` VARCHAR(60) NOT NULL,
+    `organising_body` VARCHAR(60) NOT NULL,
+    `location` VARCHAR(60) NOT NULL,
+    `primary_attribution` INT NOT NULL,
+    `event_type` VARCHAR(30) NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY funding_to_grant(`primary_attribution`)
+    REFERENCES `Grant`(`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+    FOREIGN KEY presentation_to_event_type(`event_type`)
+    REFERENCES `EventTypes`(`name`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+CREATE TABLE `EventTypes`
+(
+    `id` INT AUTO_INCREMENT,
+    `name` VARCHAR(30) NOT NULL,
+    PRIMARY KEY (`id`)
+);
