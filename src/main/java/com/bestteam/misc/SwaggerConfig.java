8e7c968c20eccfaf52475e5fb34a56c0402adc47
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2019-02-12 00:47:41 +0000

    Excluding /errors and removed swagger ui

--- src/main/java/com/bestteam/misc/SwaggerConfig.java
+++ src/main/java/com/bestteam/misc/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.bestteam.misc;
 
+import com.google.common.base.Predicates;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,9 +17,9 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-            .select()
-            .apis(RequestHandlerSelectors.any())
-            .paths(PathSelectors.any())
-            .build();
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(Predicates.not(PathSelectors.regex("/error")))
+        .build();
     }
 }
