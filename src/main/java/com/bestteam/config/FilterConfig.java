29c861b6d6c3ef09dd012274cea202a68ebe3e2b
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2019-02-20 20:29:20 +0000

    Global cors header adding

--- src/main/java/com/bestteam/config/FilterConfig.java
+++ src/main/java/com/bestteam/config/FilterConfig.java
@@ -1,5 +1,6 @@
 package com.bestteam.config;
 
+import com.bestteam.middleware.CORS;
 import com.bestteam.middleware.JWTAuth;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -12,6 +13,15 @@ public class FilterConfig {
     public FilterRegistrationBean<JWTAuth> authFilter() {
         FilterRegistrationBean<JWTAuth> bean = new FilterRegistrationBean<>();
         bean.setFilter(new JWTAuth());
+        bean.setOrder(1);
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<CORS> setCors() {
+        FilterRegistrationBean<CORS> bean = new FilterRegistrationBean<>();
+        bean.setFilter(new CORS());
+        bean.setOrder(0);
         return bean;
     }
 }
---
+++ src/main/java/com/bestteam/middleware/CORS.java
@@ -0,0 +1,18 @@
+package com.bestteam.middleware;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class CORS extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        chain.doFilter(request, response);
+    }
+}
