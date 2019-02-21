17bf790c035c5ecc3b09e27d3d7f55254e5dee18
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2019-02-21 20:07:30 +0000

    Added teammember

--- src/main/java/com/bestteam/models/TeamMember.java
+++ src/main/java/com/bestteam/models/TeamMember.java
@@ -1,23 +1,84 @@
 package com.bestteam.models;
 
 import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import java.util.Date;
 
 import javax.persistence.Column;
-
+import javax.persistence.EmbeddedId;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Table(name="Grants")
+@Table(name="TeamMember")
 public class TeamMember {
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id;
+    @EmbeddedId
+    private TeamMemberIdentity identity;
 
-    
+    @NotNull
+    @Column(name="start_date")  
+    @Temporal(TemporalType.DATE)
+    private Date startDate;
+
+    @NotNull
+    @Column(name="end_date")  
+    @Temporal(TemporalType.DATE)
+    private Date endDate;
+
+    @NotNull
+    @Column(name="role")
+    private String role;
+
+    public TeamMember() {}
+
+    public TeamMember(TeamMemberIdentity identity, Date startDate, Date endDate, String role) {
+        this.identity = identity;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.role = role;
+    }
+
+    public TeamMemberIdentity getIdentity() {
+        return this.identity;
+    }
+
+    public void setIdentity(TeamMemberIdentity identity) {
+        this.identity = identity;
+    }
+
+    public Date getStartDate() {
+        return this.startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return this.endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " identity='" + getIdentity() + "'" +
+            ", startDate='" + getStartDate() + "'" +
+            ", endDate='" + getEndDate() + "'" +
+            ", role='" + getRole() + "'" +
+            "}";
+    }
 }
--- src/main/java/com/bestteam/models/TeamMemberIdentity.java
+++ src/main/java/com/bestteam/models/TeamMemberIdentity.java
@@ -2,7 +2,52 @@ package com.bestteam.models;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.GeneratedValue;
+import javax.validation.constraints.NotNull;
+import javax.persistence.GenerationType;
 
+@SuppressWarnings("serial")
+@Embeddable
 public class TeamMemberIdentity implements Serializable {
+    @NotNull
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
 
+    @NotNull
+    @Column(name="user_id")
+    private Long userId;
+
+    public TeamMemberIdentity() {}
+
+    public TeamMemberIdentity(Long id, Long userId) {
+        this.id = id;
+        this.userId = userId;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return this.userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", userId='" + getUserId() + "'" +
+            "}";
+    }
 }
