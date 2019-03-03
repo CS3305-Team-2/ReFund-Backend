5fba998263085b0eeb485dd3bfbe750e8b0baad2
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2019-03-03 00:24:50 +0000

    Added description to project

--- src/main/java/com/bestteam/models/Project.java
+++ src/main/java/com/bestteam/models/Project.java
@@ -27,6 +27,9 @@ public class Project {
     @Column(name="name")
     private String name;
 
+    @Column(name="description")
+    private String description;
+
     @Column(name="is_research_center")
     private Boolean isResearchCenter;
 
@@ -63,6 +66,14 @@ public class Project {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public Boolean isIsResearchCenter() {
